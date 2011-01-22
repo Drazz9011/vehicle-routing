b0396f16e6b3d7672ec3a911c5687870c59861e6
lars.wesemann@googlemail.com <lars.wesemann@googlemail.com@81aea789-584e-d50a-dd90-08a89260a64e>
2011-01-22 18:35:21 +0000

    This is the Project I working on at the time. - I will now work on the output of the algorithm. So I can put the data in a diagram - The next steps will be to  restructure the code, make it more comfortable and easier to read.

---
+++ branches/SinglePeriodicVRP/SinglePeriodicVRP/src/de/wesemann/spvrp/calculation/Population.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package de.wesemann.spvrp.calculation;
+
+/**
+ * @author spinner0815
+ * 
+ */
+public class Population {
+	/*
+	 * TODO Alle Individuen hier rein - hinterlegen des besten/schlechtesten Indis - gesamte Fitness
+	 * - .....
+	 */
+}
